37a946bd7823035cef1044fd2832258f44da243e
B0224241 <b0224241@LTB0224241-Mac.local>
2023-01-18 23:02:45 +0530

    reducing rampup users

--- src/main/java/com.airtel/global/PerfParams.java
+++ src/main/java/com.airtel/global/PerfParams.java
@@ -2,7 +2,7 @@ package com.airtel.global;
 
 public class PerfParams {
     public static final int atOnceUsers=Integer.parseInt(System.getProperty("AtOnceUsers","5"));
-    public static final int rampUpUsers=Integer.parseInt(System.getProperty("RampUpUsers","100"));
+    public static final int rampUpUsers=Integer.parseInt(System.getProperty("RampUpUsers","50"));
     public static final int duration=Integer.parseInt(System.getProperty("Duration","20"));
 
 }
