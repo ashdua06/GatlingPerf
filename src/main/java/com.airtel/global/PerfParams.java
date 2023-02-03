67a2e50d7a102d7ba159102b357d2208ecd54b52
B0224241 <b0224241@LTB0224241-Mac.local>
2023-02-03 17:09:16 +0530

    common framework extracted

--- src/main/java/com.airtel/global/PerfParams.java
+++ src/main/java/com.airtel/global/PerfParams.java
@@ -4,5 +4,4 @@ public class PerfParams {
     public static final int atOnceUsers=Integer.parseInt(System.getProperty("AtOnceUsers","5"));
     public static final int rampUpUsers=Integer.parseInt(System.getProperty("RampUpUsers","10"));
     public static final int duration=Integer.parseInt(System.getProperty("Duration","10"));
-
 }
